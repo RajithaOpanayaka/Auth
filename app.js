1e877084020d445a348eb41271f3fc07d202e4de
Rajitha Opanayaka <rajithaopanayaka.ro@gmail.com>
2020-03-15 23:04:23 +0530

    add db app.js

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.set('view engine','ejs');
 app.get("/",function(req,res){
     res.render("home");
 });
+app.get("/secret",function(req,res){
+    res.render("secret");
+})
 
 app.listen(3000,function(){
     console.log("Server started");
--- db/index.js
+++ db/index.js
@@ -7,10 +7,12 @@ var con=mysql.createConnection({
     database:"nodejs"
 });
 
-con.connect(function(err){
-    if(err)throw err;
-    con.query("SELECT * FROM users",function(err,result,fields){
-        if(err)throw err;
-        console.log(result);
-    });
-});
+// con.connect(function(err){
+//     if(err)throw err;
+//     con.query("SELECT * FROM users",function(err,result,fields){
+//         if(err)throw err;
+//         console.log(result);
+//     });
+// });
+
+module.exports=con;
