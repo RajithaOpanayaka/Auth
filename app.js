b43c78a36fa5505d26346c15f17190c62bab1a34
Rajitha Opanayaka <rajithaopanayaka.ro@gmail.com>
2020-03-15 23:23:47 +0530

    add passport modules

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 var express=require("express");
+var connection=require("./db/index");
+var passport=require("passport"),
+    bodyParser=require("body-parser"),
+    LocalStrategy=require("passport-local");
 var app=express();
 
 app.set('view engine','ejs');
+app.use(require("express-session")({
+    secret:"auth using passport",
+    resave:false,
+    saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 app.get("/",function(req,res){
     res.render("home");
 });
 app.get("/secret",function(req,res){
     res.render("secret");
-})
+});
+
 
 app.listen(3000,function(){
     console.log("Server started");
