31745be6a36660e50a4fb31a737b7475f0102a9f
Rajitha Opanayaka <rajithaopanayaka.ro@gmail.com>
2020-03-16 12:24:50 +0530

    add login register

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-var express=require("express");
-var connection=require("./db/index");
-var passport=require("passport"),
+var express=require("express"),
+    connection=require("./db/index"),
+    passport=require("passport"),
     bodyParser=require("body-parser"),
-    LocalStrategy=require("passport-local");
-var app=express();
+    LocalStrategy=require("passport-local"),
+    app=express();
 
 app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(require("express-session")({
     secret:"auth using passport",
     resave:false,
@@ -15,12 +16,29 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-
+////ROUTES
 app.get("/",function(req,res){
     res.render("home");
 });
 app.get("/secret",function(req,res){
     res.render("secret");
+});
+
+///login
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.post("/login",function(req,res){
+    res.send("log in route");
+});
+
+///register
+app.get("/register",function(req,res){
+    res.render("register");
+});
+app.post("/register",function(req,res){
+    res.send("register route");
 });
 
 
